fe928951e07419f01d45965bc5919db62e026d77
Tamir <tamir.gluzman@post.runi.ac.il>
2024-02-06 16:41:50 +0200

    First version after testing

--- Track.java
+++ Track.java
@@ -17,7 +17,7 @@ class Track {
     public String toString() {
         //// Replace the following statement with code that returns
         //// the data of this track according to the method's documentation.
-        return artist + ", " + title + ", " + duration;
+        return artist + ", " + title + ", " + formattedDuration(duration);
     }
 
     /** Returns this track's title. */
@@ -42,7 +42,9 @@ class Track {
     // Returns a string that represents the totalSeconds as "minutes:seconds",
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
-        //// replace the following statement with your code
-        return "";
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+        String secondsStr = Integer.toString(seconds);
+        return minutes + ":" + (secondsStr.length() == 1 ? "0" + secondsStr : secondsStr);
     }
 }
